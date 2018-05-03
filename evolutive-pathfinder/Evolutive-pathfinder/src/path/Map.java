6ed127a7d7a7874d0ae9a699a5a4f5e743a61b44
Miguel Malaca <migue@r165144.olydorf.swh.mhn.de>
2018-05-03 02:02:27 +0100

    Finished everything responsible for creating the map. Need to test it. Tostas mistas

--- evolutive-pathfinder/Evolutive-pathfinder/src/path/Map.java
+++ evolutive-pathfinder/Evolutive-pathfinder/src/path/Map.java
@@ -3,13 +3,13 @@ package path;
 import java.awt.Point;
 import java.util.LinkedList;
 
-class Map {
+public class Map {
 	int length, width;
 	Point initialPoint;
 	Point finalPoint;
 	LinkedList<LinkedList<Edge>> grid;
 	
-	Map(int n, int m,  Point i , Point f, LinkedList<SpcZone> zones, LinkedList<Point> obstacles){
+	public Map(int n, int m,  Point i , Point f, LinkedList<SpcZone> zones, LinkedList<Point> obstacles){
 		this.length = n;
 		this.width = m;
 		this.initialPoint = i;
@@ -21,8 +21,8 @@ class Map {
 	private void fillGrid (LinkedList<SpcZone> zones, LinkedList<Point> obstacles) {
 		this.grid = new LinkedList<LinkedList<Edge>>();
 		
-		for (int i = 0 ; i < this.length ; i++) {
-			for (int j = 0 ; j < this.width ; j++) {
+		for (int i = 1 ; i <= this.length ; i++) {
+			for (int j = 1 ; j <= this.width ; j++) {
 				
 				LinkedList<Edge> aux = new LinkedList<Edge>();
 				
@@ -35,7 +35,7 @@ class Map {
 		}
 		
 		for (int i = 0 ; i < zones.size(); i++) {
-			
+			changeCost(zones.get(i));
 		}
 		
 	}
@@ -48,19 +48,50 @@ class Map {
 		// Right edge, if it exists
 		if(inMap(i+1, j) && !obstacles.contains(new Point(i+1, j))) aux.addLast(new Edge(i+1, j, 1));
 				
-		// Upper edge, if it exists
+		// Down edge, if it exists
 		if(inMap(i, j-1) && !obstacles.contains(new Point(i, j-1))) aux.addLast(new Edge(i, j-1, 1));
 		
-		// Down edge, if it exists
+		// Upper edge, if it exists
 		if(inMap(i, j+1) && !obstacles.contains(new Point(i, j+1))) aux.addLast(new Edge(i, j+1, 1));
 	}
 	
 	private boolean inMap (int i, int j) {
-		if ( i >= 0 && i <= this.length && j >= 0 && j < this.width) return true;
+		if ( i >= 1 && i <= this.length && j >= 1 && j <= this.width) return true;
 		return false;
 	}
 	
 	private void changeCost(SpcZone zone) {
 		
+		Point aux;
+		
+		for (int i = 0 ; i < 4 ; i++) {
+			aux = zone.edges.get(i);
+			for(int j = 0 ; j < this.grid.get(getInd(aux)).size() ; j++) {
+				if (this.grid.get(getInd(aux)).get(j).coord.equals(zone.edges.get((i+1)%4))) {
+					this.grid.get(getInd(aux)).get(j).cost = zone.cost;
+				}
+				if (this.grid.get(getInd(aux)).get(j).coord.equals(zone.edges.get((i == 0) ? 3 : i-1))) {
+					this.grid.get(getInd(aux)).get(j).cost = zone.cost;
+				}
+			}
+		}
+	}
+	
+	private int getInd(Point p) {
+		return (p.x-1)%this.length + (p.y-1)/this.width;
+	}
+	
+	public static void main(String[] args) {
+		int n = 5, m = 4;
+		Point i = new Point(1,1);
+		Point f = new Point(5,4);
+		LinkedList<SpcZone> z = new LinkedList<SpcZone>();
+		LinkedList<Point> o = new LinkedList<Point>();
+		
+		Map map = new Map(n, m, i, f, z, o);
+		
+		
+
 	}
 }
+
