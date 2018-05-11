e84b3dad7e508fd07d423c82df27da99d77ed73c
Miguel Malaca <migue@r165144.olydorf.swh.mhn.de>
2018-05-11 17:02:13 +0100

    Final

--- evolutive-pathfinder/Evolutive-pathfinder/src/path/Path.java
+++ evolutive-pathfinder/Evolutive-pathfinder/src/path/Path.java
@@ -169,56 +169,5 @@ public class Path implements Organism<Path> {
         System.out.println(getFitness());
         
     }
-    
-    public static void main(String[] args) {
-		int n = 5, m = 4;
-		Point i = new Point(1,1);
-		Point f = new Point(5,4);
-		LinkedList<SpcZone> z = new LinkedList<SpcZone>();
-		LinkedList<Point> o = new LinkedList<Point>();
-		
-		z.add(new SpcZone(new Point(1, 1), new Point(5,4), 4));
-		o.add(new Point(2, 3));
-		
-		
-		Map map = new Map(n, m, 4, 1, i, f, z, o);
-		
-		for (int c1 = 0 ; c1 < map.grid.size(); c1++) {
-			LinkedList<Edge> aux = map.grid.get(c1);
-			for (int c2 = 0 ; c2 < aux.size(); c2++) {
-				Edge aux2 = aux.get(c2);
-				System.out.println(aux2.coord + "  " + aux2.cost + "  " + c2 + "  " + c1);
-			}
-		}
-		
-		Path p = new Path(map);
-		System.out.println(p + " " + p.cost + " " +  p.getFitness());
-		p.mutate();
-		System.out.println(p + " " + p.cost + " " +  p.getFitness());
-		p.mutate();
-		System.out.println(p + " " + p.cost + " " +  p.getFitness());
-		p.mutate();
-		System.out.println(p + " " + p.cost + " " +  p.getFitness());
-		p.mutate();
-		System.out.println(p + " " + p.cost + " " +  p.getFitness());
-		p.mutate();
-		System.out.println(p + " " + p.cost + " " +  p.getFitness());
-		p.mutate();
-		System.out.println(p + " " + p.cost + " " +  p.getFitness());
-		p.mutate();
-		System.out.println(p + " " + p.cost + " " +  p.getFitness());
-		p.mutate();
-		System.out.println(p + " " + p.cost + " " +  p.getFitness());
-		p.mutate();
-		System.out.println(p + " " + p.cost + " " +  p.getFitness());
-		p.mutate();
-		System.out.println(p + " " + p.cost + " " +  p.getFitness());
-		p.mutate();
-		System.out.println(p + " " + p.cost + " " +  p.getFitness());
-		p.mutate();
-		System.out.println(p + " " + p.cost + " " +  p.getFitness());
-		Path s = (Path) p.reproduce();
-		System.out.println(s + " " + s.cost + " " + s.getFitness());
-		
-	}
+ 
 }
